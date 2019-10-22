85ce19d07146350e976cd17e0359e508553e8940
haihong.wang <296701193@qq.com>
2019-10-22 11:07:19 +0800

    Initial commit

---
+++ src/components/Ping.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Ping',
+  data () {
+    return {
+      msg: 'Hello, this is Ping.vue component'
+    }
+  },
+  methods: {
+    getMessage () {
+      const path = 'http://127.0.0.1:8000/api/ping'
+      axios.get(path)
+        .then((res) => {
+          this.msg = res.data
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    }
+  },
+  created () {
+    this.getMessage()
+  }
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
